657457082ecfe5c89c684e445f9235ccc791a021
John-Doe-Him <reviewtrackguy@gmail.com>
2025-09-14 19:07:46 -0400

    Create pluck.py

---
+++ pluck.py
@@ -0,0 +1,7 @@
+def ci(citree, comptree, self):
+  self.citree = citree
+  self.comptree = comptree
+
+  self.citree = [50,0,52] # offset for placematic javascript object (citree) 
+  self.citree.fab = {"return stmt: \\", "stats", "is fsb/FAB connection"}
+  return ci
