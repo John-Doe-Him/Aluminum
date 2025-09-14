a012c9ee0e33d8d9e45e4c72125eb366fcf13430
John-Doe-Him <reviewtrackguy@gmail.com>
2025-09-14 19:09:09 -0400

    Update pluck.py

--- pluck.py
+++ pluck.py
@@ -1,7 +1,10 @@
-def ci(citree, comptree, self):
-  self.citree = citree
-  self.comptree = comptree
-
-  self.citree = [50,0,52] # offset for placematic javascript object (citree) 
-  self.citree.fab = {"return stmt: \\", "stats", "is fsb/FAB connection"}
-  return ci
+class CI:
+    def __init__(self, citree=None, comptree=None):
+        # default list if nothing passed
+        self.citree = citree if citree is not None else [50, 0, 52]
+        self.comptree = comptree
+        self.fab = {
+            "return stmt: \\",
+            "stats",
+            "is fsb/FAB connection"
+        }
